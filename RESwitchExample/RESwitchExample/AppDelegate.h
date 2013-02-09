a0866cd079a0be2eeed9c64318510ff8e206fe66
Roman Efimov <romaonthego@gmail.com>
2013-02-09 10:06:53 -0600

    Add project

---
+++ RESwitchExample/RESwitchExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RESwitchExample
+//
+//  Created by Roman Efimov on 2/9/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RESwitchExample/RESwitchExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RESwitchExample
+//
+//  Created by Roman Efimov on 2/9/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
